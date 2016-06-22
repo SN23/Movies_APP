461dcf2a3b78fe5c4b8ee66ae94a6b2532600a18
Sukhjinder <nahal.snahal@gmail.com>
2016-06-22 14:55:57 -0400

    Initial commit

---
+++ app/src/main/java/com/sukhjinder/movies/DetailActivity.java
@@ -0,0 +1,12 @@
+package com.sukhjinder.movies;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Sukhjinder on 6/22/16.
+ */
+public class DetailActivity extends Fragment {
+
+
+
+}
---
+++ app/src/main/java/com/sukhjinder/movies/MainDiscoveryFragment.java
@@ -0,0 +1,19 @@
+package com.sukhjinder.movies;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Sukhjinder on 6/22/16.
+ */
+public class MainDiscoveryFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
+
