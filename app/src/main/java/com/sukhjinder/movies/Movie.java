3c0d1fd4dd9d20654e56e2973a01866324cd5987
Sukhjinder <nahal.snahal@gmail.com>
2018-01-31 16:41:46 -0500

    Changed Movie domain class to parceable and added MovieResults

--- app/src/main/java/com/sukhjinder/movies/Movie.java
+++ app/src/main/java/com/sukhjinder/movies/Movie.java
@@ -1,57 +1,203 @@
 package com.sukhjinder.movies;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Sukhjinder on 7/9/16.
  */
-public class Movie implements Serializable {
-    private String overview;
+public class Movie implements Parcelable {
+
+    private int vote_count;
+    private int id;
+    private boolean video;
+    private double vote_average;
     private String title;
-    private String poster;
-    private String backdrop;
-    private String id;
-    private String trailer;
-    private String releaseDate;
+    private double popularity;
+    private String poster_path;
+    private String original_language;
+    private String original_title;
+    private String backdrop_path;
+    private boolean adult;
+    private String overview;
+    private String release_date;
+    private List<Integer> genre_ids;
 
+    public int getVote_count() {
+        return vote_count;
+    }
 
-    public String getOverview() {
-        return overview;
+    public void setVote_count(int vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public double getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(double vote_average) {
+        this.vote_average = vote_average;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getPoster() {
-        return poster;
-    }
-
-    public String getBackdrop() {
-        return backdrop;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getTrailer() {
-        return trailer;
-    }
-
-    public String getReleaseDate() {
-        return releaseDate;
-    }
-
-
-    public Movie(String title, String overview, String poster, String backdrop, String id, String trailer, String releaseDate) {
-        this.overview = overview;
+    public void setTitle(String title) {
         this.title = title;
-        this.poster = poster;
-        this.backdrop = backdrop;
-        this.trailer = trailer;
-        this.id = id;
-        this.releaseDate = releaseDate;
-
     }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public String getBackdrop_path() {
+        return backdrop_path;
+    }
+
+    public void setBackdrop_path(String backdrop_path) {
+        this.backdrop_path = backdrop_path;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public List<Integer> getGenre_ids() {
+        return genre_ids;
+    }
+
+    public void setGenre_ids(List<Integer> genre_ids) {
+        this.genre_ids = genre_ids;
+    }
+
+    protected Movie(Parcel in) {
+        vote_count = in.readInt();
+        id = in.readInt();
+        video = in.readByte() != 0x00;
+        vote_average = in.readDouble();
+        title = in.readString();
+        popularity = in.readDouble();
+        poster_path = in.readString();
+        original_language = in.readString();
+        original_title = in.readString();
+        backdrop_path = in.readString();
+        adult = in.readByte() != 0x00;
+        overview = in.readString();
+        release_date = in.readString();
+        if (in.readByte() == 0x01) {
+            genre_ids = new ArrayList<Integer>();
+            in.readList(genre_ids, Integer.class.getClassLoader());
+        } else {
+            genre_ids = null;
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(vote_count);
+        dest.writeInt(id);
+        dest.writeByte((byte) (video ? 0x01 : 0x00));
+        dest.writeDouble(vote_average);
+        dest.writeString(title);
+        dest.writeDouble(popularity);
+        dest.writeString(poster_path);
+        dest.writeString(original_language);
+        dest.writeString(original_title);
+        dest.writeString(backdrop_path);
+        dest.writeByte((byte) (adult ? 0x01 : 0x00));
+        dest.writeString(overview);
+        dest.writeString(release_date);
+        if (genre_ids == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(genre_ids);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
---
+++ app/src/main/java/com/sukhjinder/movies/MovieResults.java
@@ -0,0 +1,79 @@
+package com.sukhjinder.movies;
+
+import java.util.List;
+
+/**
+ * Created by Sukhjinder on 1/31/18.
+ */
+
+public class MovieResults {
+    
+    private int page;
+    private int total_results;
+    private DatesBean dates;
+    private int total_pages;
+    private List<Movie> results;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public DatesBean getDates() {
+        return dates;
+    }
+
+    public void setDates(DatesBean dates) {
+        this.dates = dates;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public static class DatesBean {
+
+        private String maximum;
+        private String minimum;
+
+        public String getMaximum() {
+            return maximum;
+        }
+
+        public void setMaximum(String maximum) {
+            this.maximum = maximum;
+        }
+
+        public String getMinimum() {
+            return minimum;
+        }
+
+        public void setMinimum(String minimum) {
+            this.minimum = minimum;
+        }
+    }
+
+}
