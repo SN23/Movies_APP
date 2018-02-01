cc7b1018a9475d58c41ac68ff004e36fe417a500
Sukhjinder <nahal.snahal@gmail.com>
2018-02-01 18:07:15 -0500

    moved tabs to the bottom

--- app/src/main/java/com/sukhjinder/movies/MainActivity.java
+++ app/src/main/java/com/sukhjinder/movies/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
