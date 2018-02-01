c73eaf1fc71a01ee9191c348fbeccacd35738b6b
Sukhjinder <nahal.snahal@gmail.com>
2018-02-01 17:18:19 -0500

    added moviePoster field

--- app/src/main/java/com/sukhjinder/movies/MovieInfo.java
+++ app/src/main/java/com/sukhjinder/movies/MovieInfo.java
@@ -1,7 +1,5 @@
 package com.sukhjinder.movies;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -18,11 +16,15 @@ import com.squareup.picasso.Picasso;
 
 public class MovieInfo extends AppCompatActivity {
 
-    TextView movieOverview;
-    TextView movieReleaseDate;
-    ImageView movieBackdrop;
-    FloatingActionButton playTrailerButton;
-    FloatingActionButton addToWatchListButton;
+    private TextView movieOverview;
+    private TextView movieReleaseDate;
+    private ImageView movieBackdrop;
+    private ImageView moviePoster;
+    private FloatingActionButton playTrailerButton;
+    private FloatingActionButton addToWatchListButton;
+    private Movie movie;
+    private static String BASE_URL_BACKDROP = "https://image.tmdb.org/t/p/w1280/";
+    private static String BASE_URL_POSTER = "http://image.tmdb.org/t/p/w500/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,45 +32,52 @@ public class MovieInfo extends AppCompatActivity {
         setContentView(R.layout.movie_info);
 
         movieBackdrop = (ImageView) findViewById(R.id.movieBackdrop);
+        moviePoster = findViewById(R.id.movie_poster);
         movieOverview = (TextView) findViewById(R.id.movieOverview);
         movieReleaseDate = (TextView) findViewById(R.id.movieReleaseDate);
+        addToWatchListButton = findViewById(R.id.addToWatchList);
 
 
-        final Movie movie = (Movie) getIntent().getSerializableExtra("movieInfo");
+        movie = getIntent().getParcelableExtra("movieInfo");
+
         Picasso.with(getApplicationContext())
-                .load("https://image.tmdb.org/t/p/w1280/" + movie.getBackdrop())
+                .load(BASE_URL_BACKDROP + movie.getBackdrop_path())
                 .fit()
                 .centerCrop()
                 .into(movieBackdrop);
+
+        Picasso.with(getApplicationContext())
+                .load(BASE_URL_POSTER + movie.getPoster_path())
+                .into(moviePoster);
+
         getSupportActionBar().setTitle(movie.getTitle());
 
         movieOverview.setText(movie.getOverview());
-        movieReleaseDate.setText(movie.getReleaseDate());
+        movieReleaseDate.setText(movie.getRelease_date());
         movieBackdrop.setAdjustViewBounds(true);
+        moviePoster.setAdjustViewBounds(true);
+
+        addToWatchListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("movie", movie);
+                WatchListFragment watchListFragment = new WatchListFragment();
+                watchListFragment.setArguments(bundle);
+                Toast.makeText(getApplicationContext(), "Added To Watch List", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         playTrailerButton = (FloatingActionButton) findViewById(R.id.PlayTrailer);
-        playTrailerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (movie.getTrailer().equals("")) {
-                    Toast.makeText(getApplicationContext(), "Trailer not availble", Toast.LENGTH_SHORT).show();
-                } else {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + movie.getTrailer())));
-                }
-            }
-        });
-
-
-        addToWatchListButton = (FloatingActionButton) findViewById(R.id.addToWatchList);
-        addToWatchListButton.setOnClickListener(new View.OnClickListener() {
-            WatchList watchList = new WatchList();
-
-            @Override
-            public void onClick(View v) {
-                watchList.addToList(movie);
-                Toast.makeText(getApplicationContext(), "Added To List", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+//        playTrailerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (movie.get().equals("")) {
+//                    Toast.makeText(getApplicationContext(), "Trailer not availble", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + movie.getTrailer())));
+//                }
+//            }
+//        });
     }
 }
